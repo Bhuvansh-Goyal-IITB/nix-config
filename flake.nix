289ad9c211ddb0fbd835576784768742d9cc4647
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-07-22 17:20:40 +0530

    Updated config

--- flake.nix
+++ flake.nix
@@ -35,6 +35,7 @@
                 fonts = [
                   "FiraCode"
                   "VictorMono"
+                  "Recursive"
                   "JetBrainsMono"
                 ];
               })
