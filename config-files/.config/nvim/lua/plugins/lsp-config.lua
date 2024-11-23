9cf37da128db1bc298b46d0f8eb221afeb5f3550
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-11-23 09:29:43 +0530

    neovim update

--- config-files/.config/nvim/lua/plugins/lsp-config.lua
+++ config-files/.config/nvim/lua/plugins/lsp-config.lua
@@ -26,7 +26,6 @@ return {
     vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
     vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
     vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
     vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
     vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics)
