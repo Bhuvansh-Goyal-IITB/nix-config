cd305a6b7b5b3968ce92fe6c40990b582175be28
Bhuvansh Goyal <bhuvanshgoyal2004@gmail.com>
2024-12-09 19:31:35 +0530

    updated config

--- config-files/.config/nvim/lua/config/keymaps.lua
+++ config-files/.config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,5 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>', { silent = true })
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>', { silent = true })
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>', { silent = true })
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>', { silent = true })
-
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', { silent = true })
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { silent = true })
 vim.keymap.set('n', '<leader>x', ':bdelete<CR>', { silent = true })
--- config-files/.config/nvim/lua/plugins/rustaceanvim.lua
+++ config-files/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -8,18 +8,6 @@ return {
         handlers = {
           ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' }),
           ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' }),
-          ['textDocument/diagnostic'] = function(err, result, context, config)
-            if err ~= nil and err.code == -32802 then
-              return
-            end
-            return vim.lsp.handlers['textDocument/diagnostic'](err, result, context, config)
-          end,
-          ['workspace/diagnostic'] = function(err, result, context, config)
-            if err ~= nil and err.code == -32802 then
-              return
-            end
-            return vim.lsp.handlers['workspace/diagnostic'](err, result, context, config)
-          end,
         },
         default_settings = {
           ['rust-analyzer'] = {
